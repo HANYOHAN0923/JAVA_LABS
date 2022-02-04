f15e4a976d0fe1c61d89289bd3cb6bfb39ee5f16
HANYOHAN0923 <gksdygks2124@gmail.com>
2022-02-04 10:24:08 +0900

    Ex/ImplicitConversion.java

---
+++ Chapter2/src/binary/ExplicitConversion.java
@@ -0,0 +1,30 @@
+package binary;
+
+public class ExplicitConversion {
+
+	public static void main(String[] args) {
+		
+		// bNum의 값이 이상하게 출력된다.
+		int iNum = 1000;
+		byte bNum = (byte)iNum;
+		
+		System.out.println(iNum);
+		System.out.println(bNum);
+		
+		
+		// 정수 부분 값만 가져오고, 소숫점 값은 유실된다.
+		double dNum = 3.14;
+		iNum = (int)dNum;
+		
+		System.out.println(iNum);
+		
+		
+		float fNum =  0.9F;
+		int num1 = (int)dNum +(int)fNum;
+		int num2 = (int)(dNum +fNum);
+		
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+
+}
